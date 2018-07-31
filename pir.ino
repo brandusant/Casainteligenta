3fb3ceaf34d462d10c8af8e7c6af9f759c06a08a
brandusant <40287400+brandusant@users.noreply.github.com>
2018-07-31 11:49:05 +0300

    Add files via upload

---
+++ pir.ino
@@ -0,0 +1,37 @@
+/*  
+   http://RandomNerdTutorials.com/pirsensor
+ */
+ 
+int led = 13;                
+int sensor = 2;              
+int stare = LOW;             
+int val = 0;                 
+void setup() {
+  pinMode(led, OUTPUT);      // initializare LED ca iesire
+  pinMode(sensor, INPUT);    // initializare senzor ca intrare
+
+}
+
+void loop(){
+  val = digitalRead(sensor);   
+  if (val == HIGH) 
+  {           
+    digitalWrite(led, HIGH);   
+    delay(100);                 
+    
+    if (stare == LOW) 
+    { 
+      stare = HIGH;       
+    }
+  } 
+  else 
+  {
+      digitalWrite(led, LOW); 
+      delay(100);              
+      
+      if (stare == HIGH)
+      {
+        stare = LOW;       
+      }
+  }
+}
